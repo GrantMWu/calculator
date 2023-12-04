21cebec86973d3e2018653fec0f26cac53b3f8c5
GrantMWu <gmwu812@gmail.com>
2023-12-04 10:25:18 -0600

    Add operate function
    operate function takes two numbers and an operator (which is a function passed as a param) and returns the result

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-console.log(multiply(3,5));
-console.log(divide(5,3));
+let num1 = 5;
+let num2 = 3;
+let operator = add;
+let result = operate(operator, num1, num2);
+
+console.log(result);
 
 //FUNCTIONS
 
@@ -17,6 +21,10 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1/num2
+};
+
+function operate(operator, num1, num2) {
+    return operator(num1, num2);
 };
 
 /*
