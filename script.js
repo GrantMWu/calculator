1d01ec7af32d0457d16c8f81556d39d9a4605e25
GrantMWu <gmwu812@gmail.com>
2023-12-04 10:08:29 -0600

    Add functions for basic operations
    Add functions for add, subtract, multiply, and divide

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log(multiply(3,5));
+console.log(divide(5,3));
+
+//FUNCTIONS
+
+function add(num1, num2) {
+     return num1 + num2
+};
+
+function subtract(num1, num2) {
+    return num1 - num2
+};
+
+function multiply(num1, num2) {
+    return num1 * num2
+};
+
+function divide(num1, num2) {
+    return num1/num2
+};
+
+/*
+function sum(array) {
+  return array.reduce((accumulator, current) => accumulator + current, 0);
+};
+
+function multiply(array) {
+  return array.reduce((accumulator, current) => accumulator * current, 1);
+};
+
+const power = (num1, num2) => num1 ** num2;
+*/
