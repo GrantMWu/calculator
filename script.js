9cd41339916a988ffd8c33e961ef879399f2dd6b
GrantMWu <gmwu812@gmail.com>
2023-12-05 11:50:12 -0600

    Add functions to handle the button clicking
    Add functions to handle number and operator button clicks separately. Function behaviour relies on phase of calculation which is determined by the newNumber() boolean variable

--- script.js
+++ script.js
@@ -1,47 +1,89 @@
 let buttons = document.querySelectorAll('button');
+let numbers = document.querySelectorAll('.num');
+let operators = document.querySelectorAll('.operator');
+let numDisplay = document.getElementById('num-display');
+let calcDisplay = document.getElementById('calc-display');
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {display(button.value)})
-})
+let storage;
+let num1;
+let num2;
+let operator;
+let newNumber = true;
 
+numbers.forEach(button => {
+    button.addEventListener('click', () => {
+        handleNum(button);
+    });
+});
+
+
+operators.forEach(button => {
+    button.addEventListener('click', () => {
+        handleOperator(button);
+    });
+});
 
 
 //FUNCTIONS
 
+//Math functions
 function add(num1, num2) {
      return num1 + num2
-};
+}
 
 function subtract(num1, num2) {
     return num1 - num2
-};
+}
 
 function multiply(num1, num2) {
     return num1 * num2
-};
+}
 
 function divide(num1, num2) {
     return num1/num2
-};
+}
 
 function operate(operator, num1, num2) {
     return operator(num1, num2);
-};
-
-function display(string) {
-    document.getElementById('display').innerHTML += string;
 }
 
-//function store(number)
+//Other functions
+function changeDisplay(element, string) {
+    element.innerHTML = string;
+}
 
-/*
-function sum(array) {
-  return array.reduce((accumulator, current) => accumulator + current, 0);
-};
+function handleNum(element) {
+    if (newNumber) {
+        storage = '';
+        newNumber = false;
+    }
 
-function multiply(array) {
-  return array.reduce((accumulator, current) => accumulator * current, 1);
-};
+    storage += element.value;
+    changeDisplay(numDisplay, storage);
+}
 
-const power = (num1, num2) => num1 ** num2;
-*/
+function handleOperator(element) {
+    if(!newNumber) {
+        if (!num1){
+            num1 = +storage;
+            console.log(num1);
+            operator = window[element.id];
+        } else {
+            num2 = +storage
+            console.log(num1, num2, operator);
+            num1 = operate(operator, num1, num2);
+            num2 = '';
+        }
+    }
+    
+    
+    changeDisplay(calcDisplay, `${num1} ${element.value}`);
+    changeDisplay(numDisplay, num1)
+    newNumber = true;
+}
+
+function readyForEval() {
+    if (num2) {
+        return true;
+    }
+}
